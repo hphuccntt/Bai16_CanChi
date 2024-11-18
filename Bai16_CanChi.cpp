dde977252646770030aad34c177c8df8f1179624
hphuccntt <phuc54212420014@bachkhoasaigon.edu.vn>
2024-11-18 14:49:44 +0700

    sua lai bai

--- Bai16_CanChi.cpp
+++ Bai16_CanChi.cpp
@@ -3,8 +3,6 @@ using namespace std;
 int main()
 { 
 	int socan, sochi, nam;
-	cout << "nhap so can: "; cin >> socan;
-	cout << "nhap so chi: "; cin >> sochi;
 	cout << "nhap nam: "; cin >> nam;
 	string can[] = { "Canh", "Tan", "Nham", "Quy", "Giap", "At", "Binh", "Dinh", "Mau", "Ky"};
 	string chi[] = { "Than", "Dau", "Tuat", "Hoi", "Ty", "Suu", "Dan", "Mao", "Thin", "Ty", "Ngo", "Mui" };

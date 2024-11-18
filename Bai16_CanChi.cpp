176febe2f59cf8041f1e3d6cd7bf52be79e52ff4
hphuccntt <phuc54212420014@bachkhoasaigon.edu.vn>
2024-11-18 15:06:17 +0700

    them phan for

--- Bai16_CanChi.cpp
+++ Bai16_CanChi.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 int main()
 { 
-	int socan, sochi, nam;
-	cout << "nhap nam: "; cin >> nam;
+	int socan, sochi;
 	string can[] = { "Canh", "Tan", "Nham", "Quy", "Giap", "At", "Binh", "Dinh", "Mau", "Ky"};
 	string chi[] = { "Than", "Dau", "Tuat", "Hoi", "Ty", "Suu", "Dan", "Mao", "Thin", "Ty", "Ngo", "Mui" };
-	socan = nam % 10;
-	sochi = nam % 12;
-	cout << can[socan] << "\n";
-	cout << chi[sochi] << "\n";
+	for (int nam = 2000; nam <= 2024; nam++)
+	{
+		socan = nam % 10;
+		sochi = nam % 12;
+		cout <<"Nam"<< nam <<": "<< can[socan] << "\t";
+		cout << chi[sochi] << endl;
+
+	}
 	return 0;
 }
